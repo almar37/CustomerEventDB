75d7d57e7f2848c9e25f125abff3bd5083e6102f
almar37 <126397859+almar37@users.noreply.github.com>
2023-02-28 19:46:18 +0300

    Update settings.py

--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,8 +41,8 @@ INSTALLED_APPS = [
     'playground',
     'customer',
     'users',
+    'bookings',
     'django.contrib.sessions',
-
 ]
 
 
@@ -61,7 +62,8 @@ ROOT_URLCONF = 'storefront.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
